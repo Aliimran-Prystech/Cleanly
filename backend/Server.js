bede345a8b0f0eb976c95f7fc7331af67c4603f6
Zeeshan <zeeshan.coworker@gmail.com>
2026-09-12 02:25:42 +0500

    update port

--- backend/Server.js
+++ backend/Server.js
@@ -72,7 +72,7 @@ app.use('/api/bookings', bookingRoutes);
 app.use('/api/services', serviceRoutes);
 app.use('/api/cleaners', cleanerRoutes);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
